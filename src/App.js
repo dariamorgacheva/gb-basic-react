bdbc15701330a781341c22cd0cbf98f3c2b200e8
Daria Morgacheva <74694750+dorabataeva@users.noreply.github.com>
2022-01-25 23:27:13 -0500

    homework2 - add messageList state; add form input for message, author and text;  add chat bot message response (#2)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Message from './components/message';
 
+
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [author, setAuthor] = useState('');
+  const [messageText, setMessageText] = useState('');
+
+  useEffect(() => {
+    if (messageList.length && messageList[messageList.length - 1].author) {
+      setTimeout(() => {
+        setMessageList([...messageList, { text: 'hi I am a robot' }])
+      }, 1000)
+    }
+  }, [messageList])
+
   return (
     <div>
-     <Message 
-        text="Hello world!"
-     />
+      <input 
+        type="text" 
+        onChange={(event) => setAuthor(event.target.value)} 
+        placeholder='Your Name'
+      />
+      <input 
+        type="text" 
+        onChange={(event) => setMessageText(event.target.value)} 
+        placeholder='Your Message'
+      />
+
+      <button 
+        onClick={() => setMessageList([...messageList, {author, text: messageText}])}>
+          Submit
+      </button>
+      {
+        messageList.map((message, index) => 
+          <Message 
+            author={message.author} 
+            text={message.text} 
+            key={index}
+          />
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default App;
